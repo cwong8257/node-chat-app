2a24a8c35e4112687586a1e0e7ad1afc363b74e6
Christopher Wong <cwong8257@gmail.com>
2018-05-12 20:25:18 -0400

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Christopher Wong',
+    text: 'Hi!',
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('New Message', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,11 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.on('createMessage', (message) => {
+    message.createdAt = new Date();
+    socket.emit('newMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
