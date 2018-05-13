726f747b4b763c6727c626c24bd2c46e558a1f10
Christopher Wong <cwong8257@gmail.com>
2018-05-12 21:11:34 -0400

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,12 +14,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime(),
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined the chat',
+    createdAt: new Date().getTime(),
+  });
+
   socket.on('createMessage', ({ from, text }) => {
     io.emit('newMessage', {
       from,
       text,
       createdAt: new Date().getTime(),
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from,
+    //   text,
+    //   createdAt: new Date().getTime(),
+    // });
   });
 
   socket.on('disconnect', () => {
